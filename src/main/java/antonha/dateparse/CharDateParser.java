6bacbd6c3c32899c572341a15da938d8d0205956
anton <anton.hagerstrand@gmail.com>
2023-11-08 23:58:26 +0100

    Made CharDateParser better.

--- src/main/java/antonha/dateparse/CharDateParser.java
+++ src/main/java/antonha/dateparse/CharDateParser.java
@@ -15,6 +15,8 @@
 package antonha.dateparse;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.temporal.TemporalAccessor;
@@ -23,37 +25,98 @@ import java.time.temporal.TemporalAccessor;
  * Date parser which is based on charAt() to demonstrate that it is possible to parse dates faster than the Java
  * parsers.
 
- * -- THIS CODE IS NOT ONLY A DEMONSTRATION AND SHOULD NOT BE USED IN PRODUCTION --
+ * Might be fine to use in prod - there are quite a lot of tests, and they seem to work.
  */
 public class CharDateParser {
 
     public static TemporalAccessor parse(String dateString) {
 
-        int year = charToInt(dateString.charAt(0)) * 1000 + charToInt(dateString.charAt(1)) * 100 + charToInt(dateString.charAt(2)) * 10 + charToInt(dateString.charAt(3));
-        int month = charToInt(dateString.charAt(5)) * 10 + charToInt(dateString.charAt(6));
-        int day = charToInt(dateString.charAt(8)) * 10 + charToInt(dateString.charAt(9));
-        if (dateString.length() > 10 && dateString.charAt(10) == 'T') {
-            // T
-            int hour = charToInt(dateString.charAt(11)) * 10 + charToInt(dateString.charAt(12));
-            int minute = charToInt(dateString.charAt(14)) * 10 + charToInt(dateString.charAt(15));
-            int second = charToInt(dateString.charAt(17)) * 10 + charToInt(dateString.charAt(18));
-            int millis = 0;
-            if (dateString.charAt(19) == '.') {
-                millis = charToInt(dateString.charAt(20)) * 100 + charToInt(dateString.charAt(21)) * 10 + charToInt(dateString.charAt(22));
-            }
-            return ZonedDateTime.of(
-                    year,
-                    month,
-                    day,
-                    hour,
-                    minute,
-                    second,
-                    millis * 1_000_000,
-                    ZoneOffset.UTC
-            );
-        } else {
+        int year = charToInt(dateString.charAt(0)) * 1000
+            + charToInt(dateString.charAt(1)) * 100
+            + charToInt(dateString.charAt(2)) * 10
+            + charToInt(dateString.charAt(3));
+        if(dateString.length() == 4 || dateString.charAt(4) != '-') {
+            return LocalDate.of(year, 1, 1);
+        }
+        int month = charToInt(dateString.charAt(5)) * 10
+            + charToInt(dateString.charAt(6));
+        if(dateString.length() == 7 || dateString.charAt(7) != '-') {
+            return LocalDate.of(year, month, 1);
+        }
+        int day = charToInt(dateString.charAt(8)) * 10
+            + charToInt(dateString.charAt(9));
+        if(dateString.length() == 10 || dateString.charAt(10) != 'T') {
             return LocalDate.of(year, month, day);
         }
+        int hourPart = charToInt(dateString.charAt(11));
+        if(hourPart < 0 || hourPart > 9) {
+            return LocalDate.of(year, month, day);
+        }
+        int hour = hourPart * 10 + charToInt(dateString.charAt(12));
+        if(dateString.length() == 13 || dateString.charAt(13) != ':') {
+            return withZone(
+                year, month, day, hour, 0, 0, 0,
+                dateString, 13
+            );
+        }
+        int minute = charToInt(dateString.charAt(14)) * 10
+            + charToInt(dateString.charAt(15));
+        if(dateString.length() == 16 || dateString.charAt(16) != ':') {
+            return withZone(
+                year, month, day, hour, minute, 0, 0,
+                dateString, 16
+            );
+        }
+        int second = charToInt(dateString.charAt(17)) * 10
+            + charToInt(dateString.charAt(18));
+        if(dateString.length() == 19 || dateString.charAt(19) != '.') {
+            return withZone(
+                year, month, day, hour, minute, second, 0,
+                dateString, 19
+            );
+        }
+        int nanos = 0;
+        int pos = 20;
+        while (pos < dateString.length()) {
+            int num = charToInt(dateString.charAt(pos));
+            if(num < 0 || num > 9) {
+                break;
+            }
+            nanos = nanos * 10 + num;
+            pos++;
+        }
+        int j = pos - 20;
+        while(j < 9 ) {
+            nanos *= 10;
+            j++;
+        }
+        return withZone(
+            year, month, day, hour, minute, second, nanos,
+            dateString, pos
+        );
+    }
+
+    private static TemporalAccessor withZone(
+        int year, int month, int day,
+        int hour, int minute, int second, int nanos,
+        String dateString, int pos
+    ) {
+        if(pos == dateString.length()) {
+            return LocalDateTime.of(year, month, day, hour, minute, second, nanos);
+        }
+        return ZonedDateTime.of(
+            year, month, day, hour, minute, second, nanos,
+            parseZone(dateString, pos)
+        );
+    }
+
+    private static ZoneId parseZone(String dateString, int pos) {
+        //Fast-track for UTC, since we don't need to create a substring.
+        //Would love to have slices in Java.
+        if(dateString.charAt(pos) == 'Z') {
+            return ZoneOffset.UTC;
+        }
+        return ZoneId.of(dateString.substring(pos));
     }
 
     private static int charToInt(char c) {
--- src/test/java/antonha/dateparse/CharDateParserTest.java
+++ src/test/java/antonha/dateparse/CharDateParserTest.java
@@ -1,76 +1,164 @@
 /**
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the “Software”), to deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the “Software”), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
-
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
- * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * <p>
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package antonha.dateparse;
 
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.TestFactory;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalQueries;
+import java.util.List;
 import java.util.Random;
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DynamicContainer;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 class CharDateParserTest {
 
 
-    @TestFactory
-    public Stream<DynamicTest> testParsesSameAsES() {
-        return Stream.of(
-                "2023-01-01T23:38:34.000Z",
-                "2023-01-01T06:16:12.000Z",
-                "2023-01-01T06:16:12.542Z",
-                "2023-01-01T06:16:12Z",
-                "2023-01-01Z",
-                "2023-01-01"
+  @TestFactory
+  public Stream<DynamicTest> testParsesSameAsES() {
+    return Stream.of(
+        "2023-01-01T23:38:34.000Z",
+        "2023-01-01T06:16:12.000Z",
+        "2023-01-01T06:16:12.542Z",
+        "2023-01-01T06:16:12Z",
+        "2023-01-01Z",
+        "2023-01-01"
 
-        ).map(dateString -> DynamicTest.dynamicTest(dateString, () -> {
-            Instant esParsed = toInstant(ElasticsearchParsers.doParse(dateString));
-            Instant bytesParsed = toInstant(CharDateParser.parse(dateString));
-            assertEquals(esParsed, bytesParsed);
-        }));
-    }
+    ).map(dateString -> DynamicTest.dynamicTest(dateString, () -> {
+      Instant esParsed = toInstant(ElasticsearchParsers.doParse(dateString));
+      Instant bytesParsed = toInstant(CharDateParser.parse(dateString));
+      assertEquals(esParsed, bytesParsed);
+    }));
+  }
 
 
-    private final Random random = new Random();
+  private final Random random = new Random();
+  private Instant ex = Instant.parse("2023-01-01T23:38:34.123456789Z");
 
-    @TestFactory
-    public Stream<DynamicTest> testRandomParsesSameAsES() {
-        long min = Instant.parse("0000-01-01T00:00:00Z").toEpochMilli();
-        long max = Instant.parse("9999-12-31T23:59:59.999Z").toEpochMilli();
-        return Stream.generate(
-                () -> Instant.ofEpochMilli(min + random.nextLong(max - min)).toString()
-        ).limit(200).map(dateString -> DynamicTest.dynamicTest(dateString, () -> {
-            Instant esParsed = toInstant(ElasticsearchParsers.doParse(dateString));
-            Instant bytesParsed = toInstant(CharDateParser.parse(dateString));
-            assertEquals(esParsed, bytesParsed);
-        }));
-    }
-
-    static Instant toInstant(TemporalAccessor tmp) {
-        if(tmp.isSupported(ChronoField.HOUR_OF_DAY)) {
-            return ZonedDateTime.from(tmp).toInstant();
-        } else {
-            return LocalDate.from(tmp).atStartOfDay(ZoneOffset.UTC).toInstant();
+  @TestFactory
+  @Execution(ExecutionMode.CONCURRENT)
+  public Stream<DynamicContainer> testRandomParsesSameAsES() {
+    long min = Instant.parse("0000-01-01T00:00:00Z").toEpochMilli();
+    long max = Instant.parse("9999-12-31T23:59:59.999Z").toEpochMilli();
+    return formats.stream().map(format -> {
+          DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+          return DynamicContainer.dynamicContainer(
+              format,
+              timeZones.stream().map(zone ->
+                  DynamicContainer.dynamicContainer(
+                      zone.toString(),
+                      Stream.concat(
+                          Stream.of(ex),
+                          Stream.generate(
+                              () ->
+                                  Instant.ofEpochMilli(min + random.nextLong(max - min))
+                                      .plusNanos(random.nextLong(1_000_000_000))
+                          )).limit(100).map(instant -> {
+                        String dateString = instant.atZone(zone).format(formatter);
+                        return DynamicTest.dynamicTest(dateString, () -> {
+                          Instant esParsed = toInstant(ElasticsearchParsers.doParse(dateString));
+                          Instant bytesParsed = toInstant(CharDateParser.parse(dateString));
+                          assertEquals(esParsed, bytesParsed);
+                        });
+                      })
+                  )));
         }
+    );
+  }
 
+  static List<String> formats =
+      Stream.concat(
+          Stream.of(
+              "uuuu",
+              "uuuu-MM"
+          ),
+          Stream.of(
+              "uuuu-MM-dd",
+              "uuuu-MM-dd'T'hh",
+              "uuuu-MM-dd'T'hh:mm",
+              "uuuu-MM-dd'T'hh:mm:ss",
+              "uuuu-MM-dd'T'hh:mm:ss'.'S",
+              "uuuu-MM-dd'T'hh:mm:ss'.'SS",
+              "uuuu-MM-dd'T'hh:mm:ss'.'SSS",
+              "uuuu-MM-dd'T'hh:mm:ss'.'SSSS",
+              "uuuu-MM-dd'T'hh:mm:ss'.'SSSSS",
+              "uuuu-MM-dd'T'hh:mm:ss'.'SSSSSS",
+              "uuuu-MM-dd'T'hh:mm:ss'.'SSSSSSS",
+              "uuuu-MM-dd'T'hh:mm:ss'.'SSSSSSSS",
+              "uuuu-MM-dd'T'hh:mm:ss'.'SSSSSSSSS"
+          ).flatMap(
+              dateTimeFormat -> Stream.of("", "VV", "Z").map(zone -> dateTimeFormat + zone))
+      ).toList();
+
+  static List<ZoneId> timeZones = ZoneId.getAvailableZoneIds().stream().map(ZoneId::of)
+      .toList();
+
+  static Instant toInstant(TemporalAccessor tmp) {
+    var local = localDateTime(tmp);
+    var zone = tmp.query(TemporalQueries.zone());
+    if (zone != null) {
+      return local.toInstant(zone.getRules().getOffset(local));
     }
+    return local.toInstant(ZoneOffset.UTC);
+  }
+
+  static LocalDateTime localDateTime(TemporalAccessor tmp) {
+    var date = localDate(tmp);
+    if (!tmp.isSupported(ChronoField.HOUR_OF_DAY)) {
+      return date.atStartOfDay();
+    }
+    int hour = tmp.get(ChronoField.HOUR_OF_DAY);
+    if (!tmp.isSupported(ChronoField.MINUTE_OF_HOUR)) {
+      return date.atTime(hour, 0);
+    }
+    int minute = tmp.get(ChronoField.MINUTE_OF_HOUR);
+    if (!tmp.isSupported(ChronoField.SECOND_OF_MINUTE)) {
+      return date.atTime(hour, minute);
+    }
+    int second = tmp.get(ChronoField.SECOND_OF_MINUTE);
+    if (!tmp.isSupported(ChronoField.NANO_OF_SECOND)) {
+      return date.atTime(hour, minute, second);
+    }
+    int nano = tmp.get(ChronoField.NANO_OF_SECOND);
+    return date.atTime(hour, minute, second, nano);
+  }
+
+  static LocalDate localDate(TemporalAccessor tmp) {
+    int year = tmp.get(ChronoField.YEAR);
+    if (!tmp.isSupported(ChronoField.MONTH_OF_YEAR)) {
+      return LocalDate.of(year, 1, 1);
+    }
+    int month = tmp.get(ChronoField.MONTH_OF_YEAR);
+    if (!tmp.isSupported(ChronoField.DAY_OF_MONTH)) {
+      return LocalDate.of(year, month, 1);
+    }
+    int day = tmp.get(ChronoField.DAY_OF_MONTH);
+    return LocalDate.of(year, month, day);
+  }
 
 }
